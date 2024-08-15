c0eecca23ed43b94eb38eb3b554be3860ba4b01a
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2024-08-15 14:30:34 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+
+namespace ExpressionTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var result = SomeFunction<Person>(d => d.LastName);
+        var result2 = SomeFunction<Person>(d => d.FirstName);
+
+        Console.WriteLine(result);
+        Console.WriteLine(result2);
+    }
+
+    private static string SomeFunction<IPerson>(Expression<Func<IPerson, object>> expression)
+    {
+        var fieldName = GetFieldName(expression);
+
+        //dummy count expression for demo purpose
+        return $"COUNT({fieldName})";
+    }
+
+    private static string GetFieldName(Expression expression)
+    {
+        var ex = expression.ToString();
+
+        return string.Join("/", ex.Split('.').Skip(1));
+    }
+
+    public interface IPerson
+    {
+        int Age { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+    }
+
+    public class Person : IPerson
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+    }
+}
